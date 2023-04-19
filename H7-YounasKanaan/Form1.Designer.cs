ebcbbd997640420f5d82a7e318eeecc7834a4f41
Younas Kanaan <younas.kanaan@student.ppsjo.be>
2023-04-19 11:44:46 +0200

    dit is een wezieing van de label

--- H7-YounasKanaan/Form1.Designer.cs
+++ H7-YounasKanaan/Form1.Designer.cs
@@ -29,11 +29,12 @@
         private void InitializeComponent()
         {
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(213, 85);
+            this.button2.Location = new System.Drawing.Point(193, 33);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(338, 183);
             this.button2.TabIndex = 1;
@@ -41,21 +42,34 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 303);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dit is mijn wijzeging mijn naam is younas kanaan de derde";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
